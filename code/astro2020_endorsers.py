f7a9c61cd8900ff7146960a1c79e6e6856d8623d
Alex Drlica-Wagner <kadrlica@fnal.gov>
2019-03-06 22:57:55 -0600

    merge

--- code/astro2020_endorsers.py
+++ code/astro2020_endorsers.py
@@ -7,8 +7,11 @@
 
 import pandas as pd
 import numpy as np
+from collections import OrderedDict as odict
 
-data = pd.read_csv('astro2020_endorsers.tsv', sep='\t')
+#data = pd.read_csv('astro2020_endorsers.tsv', sep='\t')
+data = pd.read_csv('data/astro2020_endorsers.csv')
+lsst = pd.read_csv('data/lsstdarkmatter_endorsers.csv')
 
 which_papers = data['Which Decadal Survey Science Submissions are you willing to endorse?']
 
@@ -17,12 +20,32 @@ cut = (count > 0)
 
 print np.sum(cut), len(which_papers)
 
-data['Latex Name'][cut]
-data['LaTeX Affiliation alias(es)'][cut]
+data = data[cut]
+merge = data.merge(lsst,left_on=['Surname','Name'],right_on=['Lastname','Firstname'])
 
-df = pd.DataFrame({'name': data['Latex Name'][cut],
-                   'affiliation': data['LaTeX Affiliation alias(es)'][cut]})
-df.to_csv('astro2020_endorsers_trimmed.csv')
+cut = ~np.in1d(data['Surname']+data['Name'],merge['Surname']+merge['Name'])
+data = data[cut]
+
+new = pd.DataFrame(odict([("Lastname", data['Surname']),
+                    ("Firstname", data['Name']),
+                    ("Authorname", data['Latex Name']),
+                    ("AuthorType", len(data)*['Supporter']),
+                    ("Affiliation", data['LaTeX Affiliation alias(es)']),
+                    ("Contribution", len(data)*['Supporter']),
+                    ("Email", data['Email Address']),
+                    ("ORCID", len(data)*['']),
+                ])).sort_values(by='Lastname')
+
+out = lsst.append(new)
+print len(lsst), len(out)
+out.to_csv('astro2020_endorsers_trimmed_merged.csv',index=False)
+
+#data['Latex Name'][cut]
+#data['LaTeX Affiliation alias(es)'][cut]
+#
+#df = pd.DataFrame({'name': data['Latex Name'][cut],
+#                   'affiliation': data['LaTeX Affiliation alias(es)'][cut]})
+#df.to_csv('astro2020_endorsers_trimmed.csv')
 
 # Open that trimmed file with pd.read_csv
 
